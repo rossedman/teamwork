bfb27e31d9d8567b7005ab7aa9a9d86e1494e66f
Ross Edman <ross.edman@gmail.com>
2015-03-03 17:32:11 -0600

    Update Company
    — Add RestfulTrait to company

---
+++ src/Rossedman/Teamwork/Company.php
@@ -0,0 +1,25 @@
+<?php  namespace Rossedman\Teamwork; 
+
+use Rossedman\Teamwork\RestfulTrait;
+
+class Company extends Object {
+
+    use RestfulTrait;
+
+    protected $wrapper = 'company';
+    protected $endpoint = 'companies';
+
+    /**
+     * Get People Associated With Company
+     * GET /companies/{company_id}/people.json
+     *
+     * @link http://developer.teamwork.com/people#get_people_(withi
+     *
+     * @return mixed
+     */
+    public function people()
+    {
+        return $this->client->get("$this->endpoint/$this->id/people")->response();
+    }
+
+}
